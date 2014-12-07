f7d59443ffacd283737690bdf1838d06d315e0bc
jagomez <jgomez.career@gmail.com>
2014-12-07 16:47:01 -0600

    adding angular for education

---
+++ app/controllers/api/v1/educations_controller.rb
@@ -0,0 +1,13 @@
+ class Api::V1::EducationsController < ApplicationController
+ 	def create
+		@education= Education.create(education_params)
+	end
+	def education_params
+		return params.require(:education).permit(:school, :year, :degree :user_id)
+	end
+	def restrict_access
+		authenticate_or_request_with_http_token do |api_key, options|
+			User.find_by(email: request.headers["X-User-Email"], api_key: api_key)
+		end
+	end
+ end
