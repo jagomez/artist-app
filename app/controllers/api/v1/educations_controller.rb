3998a1b92db94be6bf2277f62bbe9a363750fc2f
jagomez <jgomez.career@gmail.com>
2014-12-08 14:09:44 -0600

    added form to education

--- app/controllers/api/v1/educations_controller.rb
+++ app/controllers/api/v1/educations_controller.rb
@@ -3,7 +3,7 @@
 		@education= Education.create(education_params)
 	end
 	def education_params
-		return params.require(:education).permit(:school, :year, :degree :user_id)
+		return params.require(:education).permit(:school, :year, :degree, :user_id)
 	end
 	def restrict_access
 		authenticate_or_request_with_http_token do |api_key, options|
---
+++ app/views/api/v1/educations/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.id @education.id
+json.user_id @education.user_id
+json.school @education.school
+json.year @education.year
+json.degree @education.degree
