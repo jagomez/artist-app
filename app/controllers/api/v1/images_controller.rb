8951542d9ddec4a09b84d45583a1b1d7b607a34d
jagomez <jgomez.career@gmail.com>
2014-12-09 20:44:21 -0600

    adding form to images

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::ImagesController < ApplicationController
  	def create
-		@image= image.create(image_params)
+		@image= Image.create(image_params)
 	end
 	
 	def image_params
-		return params.require(:image).permit( :image, :title, :medium, :medium, :year, :description, :size, :user_id)
+		return params.require(:image).permit( :image, :title, :medium, :year, :description, :size, :user_id)
 	end
 	
 	def restrict_access
