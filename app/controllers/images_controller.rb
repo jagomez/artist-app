8101dfe75b0c843373e27e498bac9f42cb187a99
jagomez <jgomez.career@gmail.com>
2014-12-09 21:35:14 -0600

    adding forms

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,6 +5,16 @@ class ImagesController < ApplicationController
 	def new
 		@image= Image.new
 	end
+	def create
+		@image = Image.new(image_params)
+		if @image.save
+			flash[:success] = "Image has been added."
+	    	redirect_to image_path(@image.id)
+		else
+		render 'new'
+		end
+	end 
+
 	def show
 		@image = Image.find_by(:id=>params[:id])
 	end
@@ -13,3 +23,4 @@ class ImagesController < ApplicationController
 		return params.require(:image).permit(:image, :title, :medium, :year, :description, :size)	
 	end
 end
+
