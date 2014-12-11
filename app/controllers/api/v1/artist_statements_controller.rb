1d7c4c9f84a8422ee2bf9769134c7b5a235c41b4
jagomez <jgomez.career@gmail.com>
2014-12-11 14:19:20 -0600

    adding angular to artist statements

---
+++ app/controllers/api/v1/artist_statements_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::ArtistStatementsController < ApplicationController
+	def create
+		@artist_statement= artist_statement.create(statement_params)
+	end
+	def statement_params
+		return params.require(:statement).permit(:description, :statement, :user_id)
+	end
+	def restrict_access
+		authenticate_or_request_with_http_token do |api_key, options|
+		User.find_by(email: request.headers["X-User-Email"], api_key: api_key)
+	end
+	end
+end
