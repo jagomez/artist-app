3fc24f2090dbfc6cb4a323664aa7115bfec538e2
jagomez <jgomez.career@gmail.com>
2014-12-15 11:25:53 -0600

    updating resume pdf

--- app/pdfs/resume_pdf.rb
+++ app/pdfs/resume_pdf.rb
@@ -2,26 +2,42 @@ class ResumePdf < Prawn::Document
 
   def initialize(user, view)
     super()
-    text "Name: #{user.full_name}"
-    stroke_axis
-    	stroke_color "#BFEFFF"
-    	self.line_width = 2 
+    font "Times-Roman"
+    font_size 20
+    move_down 40
+    text "#{user.full_name}", :align => :center
+    font_size 14
+    text "#{user.address}", :align => :center
+    text "#{user.city} #{user.state} #{user.zip_code}", :align => :center
+    text "#{user.email}", :align => :center
+    text "#{user.website}", :align => :center
+    text "#{user.phone}", :align => :center
+    
+    stroke
+    	stroke_color "#000000"
+    	self.line_width = 0.5 
     	self.cap_style = :round
     	stroke do
-    		line [400, 670], [50, 670]
-
+    		line [500, 570], [0, 570]
+    end
     	
-	end
-    text "Education:"
+	
+    move_down 25
+    text "Education:", :align => :left, :leading => 5, :styles => :bold
     user.educations.each do |education|
-    	text education.school
-    	text education.degree
-    	text education.year
+    	education.school
+    	education.degree
+    	education.year
+        text "#{education.school}, #{education.degree}, #{education.year}", :leading => 2
     end
-    text "Past Shows:"
+
+    move_down 25
+    text "Past Shows:", :align => :left, :leading => 5
     user.experiences.each do |experience|
-    	text experience.year
-    	text experience.title
+    	experience.year
+    	experience.title
+        text "#{experience.year}, #{experience.title}", :align => :left, :leading => 2
     end
+    
   end
 end
