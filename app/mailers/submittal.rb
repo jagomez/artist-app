dd3c749e335e7833e47c1f543ccfe141a059d0ae
jagomez <jgomez.career@gmail.com>
2014-12-16 19:16:59 -0600

    adding attachments to emails

--- app/mailers/submittal.rb
+++ app/mailers/submittal.rb
@@ -1,14 +1,18 @@
 class Submittal < ActionMailer::Base
+  require 'open-uri'
   default from: "jgomez.artist@gmail.com"
 
     def submittal_email(artist_statement, images, resume, opportunities, user, view)
     @artist_statement = artist_statement
     @user = user
-    @images = images
+    @images = user.images
     @resume = resume
     @opportunity = opportunities
     email_with_name = "jgomez.artist@gmail.com"
-    attachments['pastels_lores.jpg'] = File.read('app/assets/images/pastels_lores.jpg')
+    @images.each do |image|
+      attachments[image.image.file.filename] = open(image.image.url).read
+    end
+    attachments["resume.pdf"] = ResumePdf.new(user, view_context).render #pdf.render #, filename: "#{user.full_name}_resume.pdf", type: "application/pdf", disposition: "inline"
     mail( :to => "jgomez.artist@gmail.com",
     :subject => "Submittal for #{@opportunity.projectname}"  )
    
